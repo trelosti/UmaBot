e6890bc34f6c5a64cbdd990a06aa5cb28c4b52fc
trelosti <trelosti@gmail.com>
2022-02-20 17:19:10 +0600

    MemeParser (ignore used memes)

--- src/main/java/DTFParser/Parser.java
+++ src/main/java/DTFParser/Parser.java
@@ -13,12 +13,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.awt.*;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.sql.Time;
 import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -28,12 +22,11 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Parser extends ListenerAdapter {
-    String filePath = System.getProperty("/app") + File.separator + "resources" + File.separator + "usedMemes.txt";
-    String lineSeparator = System.lineSeparator();
+    ArrayList<String> usedMemes;
 
     public void onReady(ReadyEvent event) {
-        System.out.println(System.getProperty("user.dir"));
-        System.out.println(filePath);
+        usedMemes = new ArrayList<>();
+
         System.setProperty("GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome");
         System.setProperty("CHROMEDRIVER_PATH", "/app/.chromedriver/bin/chromedriver");
         JDA jda = event.getJDA();
@@ -59,46 +52,28 @@ public class Parser extends ListenerAdapter {
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
 
-        StringBuffer buffer = new StringBuffer();
-        try {
-            readFileWithMemes(filePath, buffer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         /*  Check for a new meme once in 20 seconds */
-        ScheduledExecutorService schedulerGetMemes = Executors.newScheduledThreadPool(2);
+        ScheduledExecutorService schedulerGetMemes = Executors.newScheduledThreadPool(1);
         schedulerGetMemes.scheduleAtFixedRate(() -> {
                     WebDriver driver = new ChromeDriver(options);
                     try {
                         driver.get("https://dtf.ru/kek/entries/top/day");
                         System.out.println("Driver boot");
 
-
                         WebElement page = driver.findElement(By.cssSelector("div.feed__chunk"));
-                        ArrayList<WebElement> memes = (ArrayList<WebElement>) page.findElements(By.cssSelector("div.andropov_image"));
-                        String memeSrc = "";
-                        for (WebElement el : memes)
-                        {
-                            if (buffer.indexOf(el.getAttribute("data-image-src")) == -1)
-                            {
-                                memeSrc = el.getAttribute("data-image-src");
-                                break;
-                            }
-                        }
-                        //WebElement meme = page.findElement(By.cssSelector("div.andropov_image"));
-                        //String memSrc = meme.getAttribute("data-image-src");
+                        WebElement meme = page.findElement(By.cssSelector("div.andropov_image"));
+                        String memSrc = meme.getAttribute("data-image-src");
 
                         EmbedBuilder builder;
 
                         // Ignore if a new image is the same as a previous one
-                        if (!memeSrc.isEmpty()) {
+                        if (!memSrc.isEmpty() && !usedMemes.contains(memSrc)) {
                             builder = new EmbedBuilder()
-                                    .setImage(memeSrc)
+                                    .setImage(memSrc)
                                     .setColor(Color.GREEN);
 
                             guild.getTextChannelById("800740503914020879").sendMessage(builder.build()).queue();
-                            writeToFileWithMemes(filePath, memeSrc);
+                            usedMemes.add(memSrc);
                         }
                     } finally {
                         try {
@@ -115,38 +90,10 @@ public class Parser extends ListenerAdapter {
         );
 
         schedulerGetMemes.scheduleAtFixedRate(() -> {
-                    try {
-                        clearFileWithMemes(filePath);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    usedMemes.clear();
                 },
                 clearDelay,
                 TimeUnit.DAYS.toSeconds(2),
                 TimeUnit.SECONDS);
     }
-
-    private void readFileWithMemes(String path, StringBuffer buffer) throws IOException {
-        String line;
-        FileReader fileReader = new FileReader(path);
-        BufferedReader br = new BufferedReader(fileReader);
-        while ((line = br.readLine()) != null) {
-            buffer.append(line).append(lineSeparator);
-        }
-    }
-
-    private void writeToFileWithMemes(String pathToFile, String src) {
-        Path path = Paths.get(pathToFile);
-        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardOpenOption.APPEND)) {
-            writer.write(src + lineSeparator);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void clearFileWithMemes(String pathToFile) throws IOException {
-        Path path = Paths.get(pathToFile);
-        Files.newBufferedWriter(path, StandardOpenOption.TRUNCATE_EXISTING);
-    }
 }
-
